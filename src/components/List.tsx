880a8ae435b27f8a01480f91c4144ea1da984d5e
marktipton <marktipton95@gmail.com>
2024-09-19 11:05:42 -0500

    delete list button now appearing in middle of list on hover

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -5,9 +5,7 @@ import DeleteListButton from './DeleteListButton'
 const List = () => {
   return (
     <div className='group/list h-full min-w-96 p-4'>
-      <div className='opacity-0 group-hover:opacity-100 transition-opacity duration-300'>
-        <DeleteListButton />
-      </div>
+      <DeleteListButton />
       <h3 className='text-center'>To Do</h3>
       <Card />
       <Card />
