20702c1e9060c8bb23676213c378a4aca1dcb800
marktipton <marktipton95@gmail.com>
2024-09-23 14:15:53 -0500

    added template for cardsSlice

---
+++ src/slices/cardsSlice.ts
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from "nanoid";
+
+const initialState = {
+  cards: [
+    { id: nanoid(), title: "Lorem ipsum dolor", description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, fugiat!" },
+  ],
+};
+
+export const cardsSlice = createSlice({
+  name: "cards",
+  initialState,
+  reducers: {
+    addCard: (state, action) => {
+      state.cards.push({
+        id: nanoid(),
+        title: action.payload.title,
+        description: action.payload.description,
+      });
+    },
+    // deleteCard: (state, action) => {
+    //   state.lists = state.lists.filter((list) => list.id !== action.payload.id);
+    // },
+    // clearBoard: (state) => {
+    //   state.lists = []; // Clear all lists
+    // },
+  },
+});
+
+export const { addCard } = cardsSlice.actions;
+
+export default cardsSlice.reducer;
