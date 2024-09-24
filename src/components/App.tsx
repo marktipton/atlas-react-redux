95e066768dc86c24e85ba4ab44c4d84dbaddcd57
marktipton <marktipton95@gmail.com>
2024-09-23 20:50:48 -0500

    local storage appears to be working on inspection of application storage in dev tools

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,17 +2,19 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Board from "./Board";
 import { Provider } from "react-redux";
-import { store } from "../store";
-
+import { persistor, store } from "../store";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="">
-        <Header />
-        <Board />
-        <Footer />
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="">
+          <Header />
+          <Board />
+          <Footer />
+        </div>
+      </PersistGate>
     </Provider>
 
   );
--- src/store.ts
+++ src/store.ts
@@ -2,11 +2,23 @@ import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch, useSelector } from "react-redux";
 import listsSliceReducer from './slices/listsSlice';
 import cardsSliceReducer from './slices/cardsSlice';
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+
+const persistedListReducer = persistReducer(
+  { key: "lists", storage },
+  listsSliceReducer
+);
+
+const persistedCardReducer = persistReducer(
+  { key: "cards", storage },
+  cardsSliceReducer
+);
 
 export const store = configureStore({
   reducer: {
-    lists: listsSliceReducer,
-    cards: cardsSliceReducer,
+    lists: persistedListReducer,
+    cards: persistedCardReducer,
   },
 });
 
@@ -17,3 +29,5 @@ export type AppDispatch = typeof store.dispatch;
 //Add types to dispatch and selector hooks
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
+
+export const persistor = persistStore(store);
