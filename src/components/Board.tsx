629729bc6e498583c7e3ae0c95456a55562ab31e
marktipton <marktipton95@gmail.com>
2024-09-23 20:30:17 -0500

    adding pointer sensor fixed delete card issue. Card delete now functioning correctly

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import List from './List';
-import { DndContext, DragEndEvent } from '@dnd-kit/core';
+import { DndContext, DragEndEvent, useSensors, useSensor, PointerSensor } from '@dnd-kit/core';
 import { useAppSelector, useAppDispatch } from '../store';
 import { moveCard } from '../slices/cardsSlice';
 
@@ -8,7 +8,13 @@ const Board = () => {
   const lists = useAppSelector((state) => state.lists.lists);
   const dispatch = useAppDispatch();
 
-
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 10, // Minimum movement before drag starts
+      },
+    }),
+  );
   const handleDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
 
@@ -17,7 +23,7 @@ const Board = () => {
     }
   };
   return (
-    <DndContext onDragEnd={handleDragEnd}>
+    <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
       <div className='m-auto h-screen w-screen overflow-x-scroll text-center'>
         <div className='flex h-full space-x-4'>
           {lists.map((list) => (
